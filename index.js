3b93622b8c8107308a2ed9ce3318677929bfd025
Tanmay Shukla <tanmayshukla20@gmail.com>
2023-09-19 22:07:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const mobileNav = document.querySelector(".mobile-navbar-btn");
+const navHeader = document.querySelector(".header");
+
+function toggleNavbar() {
+    navHeader.classList.toggle("active");
+}
+
+mobileNav.addEventListener("click", () => toggleNavbar());
